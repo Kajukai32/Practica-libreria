32faec97e273285ac67346af9145d3cdf008bf6a
artur <artur@192.168.1.2>
2026-02-16 14:29:44 -0300

    commit nr1.

---
+++ PracticaLibreria/test/practicalibreriatest/PracticaLibreriaTest.java
@@ -0,0 +1,21 @@
+package practicalibreriatest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class PracticaLibreriaTest {
+
+	@Test
+	void shouldCreateABookTest() {
+		// GIVEN
+		Product b1 = new Book("Lo que el viento se llevo", "B1702", 12, 300, 1200.0);
+		// WHEN
+		// THEN
+
+		fail("not implemented yet");
+
+	}
+
+
+}
