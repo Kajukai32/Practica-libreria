d4e97e07299adfcea84abb493ec8b71c0939f1ae
artur <artur@192.168.1.2>
2026-02-16 14:42:41 -0300

    commit nro 2

--- PracticaLibreria/test/practicalibreriatest/PracticaLibreriaTest.java
+++ PracticaLibreria/test/practicalibreriatest/PracticaLibreriaTest.java
@@ -9,7 +9,7 @@ class PracticaLibreriaTest {
 	@Test
 	void shouldCreateABookTest() {
 		// GIVEN
-		Product b1 = new Book("Lo que el viento se llevo", "B1702", 12, 300, 1200.0);
+		Product b1 = new Book("Lo que el viento se llevo", 12, 1200.0, 300, "B1702");
 		// WHEN
 		// THEN
 
